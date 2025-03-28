974cfa0c09ae5f7b63d5440dc6cfeea67ad42c0c
Jonas Almeida <jalmeida@mathbiol.org>
2025-03-28 09:32:56 -0400

    matrix multiplication

--- NMF.mjs
+++ NMF.mjs
@@ -1,10 +1,23 @@
 console.log(`non-negative matrix factorization`)
 
 // matrix multiplication
-function multiply(A,B){
-    console.log(`multiplying ${A} by ${B}`)
+
+
+function multiplyMatrices(
+    A=[
+       [1, 2, 3],
+       [4, 5, 6]
+    ],
+    B=[
+       [7, 8],
+       [9, 10],
+       [11, 12],
+    ]) {
+    if(A[0].length!=B.length){
+        console.error(`wrong size, number of columns of first matrix is ${A[0].length} while number of rows of second matrix is ${B.length}`)
+    }
+    console.log(`multiplying`,A, 'by', B)
+    return A.map( (row, i) => B[0].map( (_, j) => row.reduce( (sum, _, k) => sum + A[i][k] * B[k][j], 0))) 
 }
 
-export{
-    multiply
-}
+export {multiplyMatrices}
